194e88c809f83b63da225b501f2e8f9dc7e12406
ismailkhosrowshiri <80094128+ismailkhosrowshiri@users.noreply.github.com>
2022-02-18 18:04:41 +0330

    Delete ToDo App - MVC directory

--- ToDo App - MVC/assets/js/app.js
+++
@@ -1,5 +0,0 @@
-import Controller from "./controller";
-import View from "./view";
-import Model from "./model";
-
-const app = new Controller(new View(), new Model());
--- ToDo App - MVC/assets/js/controller.js
+++
@@ -1,4 +0,0 @@
-class Controller {
-  constructor() {}
-}
-export default Controller;
